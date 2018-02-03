0158aa37ec8aacbdddc2c131aee9dccc23f6d2c4
Jason Benterou <jason.benterou@gmail.com>
2018-02-03 20:39:11 +0000

    Update to es6 and add ascii art

--- bin/tightloop.bin.js
+++ bin/tightloop.bin.js
@@ -1,41 +1,30 @@
 const ProgressBar = require('progress');
-
-
-/*
- *
- * * Behavior constants * */
-const TARGET_BATCH_DELAY = 250;
-const BATCH_MAX = 100000;
-const BATCH_INITIAL_SIZE = 2;
+const tightloop = require('../tightloop.js');
 
 
 /*
  *
  * * UI Support code * */
-class ProgressDisplay {
-    constructor() {
-        this.progressbar;
-        this.ticks_total;
-        this.ticks_done = 0;
+let ui_progressbar;
+let ui_ticks_total;
+let ui_ticks_done = 0;
+function progress(t_init, t_now, t_last) {
+    if (!ui_progressbar) {
+        const total = ui_ticks_total = Math.ceil((t_last - t_init) / TARGET_BATCH_DELAY);
+        ui_progressbar = new ProgressBar('├:bar┤ :percent :elapseds', {
+            total,
+            complete: '█',
+            incomplete: ' '
+        });
     }
-    refresh(t_init, t_now, t_last) {
-        if (!this.progressbar) {
-            const total = this.ticks_total = Math.ceil((t_last - t_init) / TARGET_BATCH_DELAY);
-            this.progressbar = new ProgressBar('├:bar┤ :percent :elapseds', {
-                total,
-                complete: '█',
-                incomplete: ' '
-            });
-        }
-        if (this.progressbar.complete) {
-            return;
-        }
-        const new_steps = Math.ceil((t_now - t_init) / TARGET_BATCH_DELAY) - this.ticks_done;
-        for (let i=0; i < new_steps; i++) {
-            this.progressbar.tick();
-        }
-        this.ticks_done += new_steps;
+    if (ui_progressbar.complete) {
+        return;
     }
+    const new_steps = Math.ceil((t_now - t_init) / TARGET_BATCH_DELAY) - ui_ticks_done;
+    for (let i=0; i < new_steps; i++) {
+        ui_progressbar.tick();
+    }
+    ui_ticks_done += new_steps;
 }
 
 
@@ -53,7 +42,6 @@ function tightloop(testCase, { duration=4000, ui=false }={}) {
      *
      * `duration` is measured in ms
      */
-    const ui_progress = ui ? new ProgressDisplay() : null;
     const t_init = now();
     const t_stop = t_init + duration;
     let batch = BATCH_INITIAL_SIZE;
@@ -69,18 +57,21 @@ function tightloop(testCase, { duration=4000, ui=false }={}) {
             batch = Math.max(1, Math.min(BATCH_MAX, batch));
             done = t_stop < t_now;
 
-            ui && ui_progress.refresh(t_init, t_now, t_stop);
+            ui && progress(t_init, t_now, t_stop);
 
             t_last = ui ? now() : t_now;
             i = 1;
         }
     }
 
+
     t_end = now();
 
+
     const report = { mean_avg_ms: (t_end - t_init) / runs, runs: runs, elapsed_ms: t_end - t_init };
-    ui && console.log(report);
+    console.log(report);
     return report;
 }
 
 module.exports = tightloop;
+
